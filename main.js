f0fcef861eec21db6a65c29ed2505bc654882d63
nick66551 <john831118@gmail.com>
2016-11-03 19:22:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ var towerbutton={
 //畫畫
 function draw(){
 
-  enemy.move();
+// enemy.move();
   ctx.drawImage(bgImg,50,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
   ctx.drawImage(towerImg,towerbutton.x,towerbutton.y,towerbutton.width,towerbutton.height);
