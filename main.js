11df16154ecb290fec7287c8aa250d76dedd7d43
nick66551 <john831118@gmail.com>
2016-11-03 20:13:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -54,12 +54,12 @@ var enemy ={
       else{
       this.speedx=0;
       this.speedy=-64;
-      }
+      }}
       else{
     this.x=this.x+this.speedx/FPS;
     this.y=this.y+this.speedy/FPS;
       }
-  }
+  
   
 
 };
